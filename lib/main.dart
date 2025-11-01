5c9a1ea363243286354c616ffff2bb25c27ec9c1
gyurza21-lang <gyurza21@gmail.com>
2025-11-01 21:54:29 +0200

    Refactor MyApp to use const constructors

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ESP32 Distiller App',
-      home: Scaffold(
-        appBar: AppBar(title: Text('ESP32 Distiller')),
-        body: Center(child: Text('Welcome to ESP32 Distiller App')),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(
+        body: Center(child: Text('ESP32 Automation App')),
       ),
     );
   }
